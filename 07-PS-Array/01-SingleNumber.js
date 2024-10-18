77628afd9a1fd7f43812a545b0bdf0f54e5c5ecd
asher9889 <saurabhkushwaha9889@gmail.com>
2024-10-18 13:08:13 +0530

    Single Number from Array

---
+++ 07-PS-Array/01-SingleNumber.js
@@ -0,0 +1,18 @@
+let nums = [4,2,1,2,1]
+/**
+output should be 4;
+If nums = [1] , Output = 1;
+Total 2 Approach
+1. By making JS Object
+2. Bit Wise XOR operator
+*/ 
+function singleNumber(arr){
+    let result = 0;
+    for(let i = 0; i < arr.length; i++){
+        result = result ^ arr[i];
+    }
+    return console.log(result)
+}
+
+singleNumber(nums)
+
